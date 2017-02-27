e869d3bef7576020a8104b958fe546ccfdd8b9e1
Igor Shapiro <shapigor@gmail.com>
2017-02-27 13:12:32 -0800

    + Implemented `config.get`

--- examples/app/test/config_test.js
+++ examples/app/test/config_test.js
@@ -2,4 +2,24 @@ describe('config', function() {
   it('loads distinct configs', function() {
     expect(app.config.yaml.key).to.eql('devValue')
   })
+
+  describe('config merge', function() {
+    it('defaults to /config/*.js', function() {
+      expect(app.config.get('yaml.nested.nested1.envSpecificKey')).to.eql('devValue')
+    })
+
+    it('deep merges configs', function() {
+      expect(app.config.get('yaml.nested.globalKey')).to.eql('globalVal')
+    })
+  })
+
+  describe('get', function() {
+    it('returns default if config value does not exist', function() {
+      expect(app.config.get('missing.key', 5)).to.eql(5)
+    })
+
+    it('returns nested values', function() {
+      expect(app.config.get('yaml.nested.nested1.key')).to.eql('nestedValue')
+    })
+  })
 })
