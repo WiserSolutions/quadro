054c578f06321e939964d771e48570efd090ccff
Igor Shapiro <shapigor@gmail.com>
2017-03-13 15:26:01 -0700

    + Fixed ESLint files pattern

--- initializers/es_lint.js
+++ initializers/es_lint.js
@@ -4,7 +4,9 @@ module.exports = async function(app, log) {
   log.trace('Running ESLint')
 
   let CLIEngine = require('eslint').CLIEngine
-  let cli = new CLIEngine({})
+  let cli = new CLIEngine({
+    ignorePath: require('path').resolve(__dirname, '../.eslintignore')
+  })
   let report = cli.executeOnFiles([app.appDir])
   let formatter = cli.getFormatter('compact')
   console.log(formatter(report.results))
