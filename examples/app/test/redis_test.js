7ae975103101c91d77b41e8543ecb570a2340d12
Igor Shapiro <shapigor@gmail.com>
2017-03-14 17:48:16 -0700

    + Added redis (resolves #25)

---
+++ examples/app/test/redis_test.js
@@ -0,0 +1,6 @@
+describe.only('Redis Service', function() {
+  it('exists', async function() {
+    let redis = container.get('redis')
+    expect(await redis.pingAsync('hello')).to.eql('hello')
+  })
+})
