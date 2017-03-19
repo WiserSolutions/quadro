5612a7727d392bff5c210aa5f57fe15d19dc9584
Igor Shapiro <shapigor@gmail.com>
2017-03-19 03:27:11 -0700

    + Added test for controller dependency injection

--- examples/app/initializers/app_func_initializer.js
+++ examples/app/initializers/app_func_initializer.js
@@ -1,3 +1,5 @@
-module.exports = function(log, app) {
+module.exports = function(log, app, container) {
   log.info(`function initializer of ${app.name}`)
+
+  container.register('userControllerDep', 'John')
 }
