0a7c29733f8a2cac5e086ee54f944acff9769363
Igor Shapiro <shapigor@gmail.com>
2017-03-21 17:52:53 -0700

    + Removed globals: app, log, config, container and introduced `Q` namespace instead

--- examples/app/test/config_test.js
+++ examples/app/test/config_test.js
@@ -1,25 +1,33 @@
 describe('config', function() {
   it('loads distinct configs', function() {
-    expect(config.yaml.key).to.eql('devValue')
+    expect(Q.config.yaml.key).to.eql('devValue')
   })
 
   describe('config merge', function() {
     it('defaults to /config/*.js', function() {
-      expect(config.get('yaml.nested.nested1.envSpecificKey')).to.eql('devValue')
+      expect(Q.config.get('yaml.nested.nested1.envSpecificKey')).to.eql('devValue')
     })
 
     it('deep merges configs', function() {
-      expect(config.get('yaml.nested.globalKey')).to.eql('globalVal')
+      expect(Q.config.get('yaml.nested.globalKey')).to.eql('globalVal')
     })
   })
 
   describe('get', function() {
     it('returns default if config value does not exist', function() {
-      expect(config.get('missing.key', 5)).to.eql(5)
+      expect(Q.config.get('missing.key', 5)).to.eql(5)
     })
 
     it('returns nested values', function() {
-      expect(config.get('yaml.nested.nested1.key')).to.eql('nestedValue')
+      expect(Q.config.get('yaml.nested.nested1.key')).to.eql('nestedValue')
     })
   })
+
+  // describe('tests', function() {
+  //   describe('stubConfig', function() {
+  //     it('stubs value', function() {
+  //       Q.stubConfig('quadro.logger.hello', 123)
+  //     })
+  //   })
+  // })
 })
