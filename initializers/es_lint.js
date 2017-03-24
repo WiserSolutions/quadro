64126c573c5bf3aac54a9bc66632a8032fc479ee
Igor Shapiro <shapigor@gmail.com>
2017-03-24 14:06:25 -0700

    ESLint fixes
    ESLint dependencies #47
    Support app-specific eslint config files #48

--- initializers/es_lint.js
+++ initializers/es_lint.js
@@ -1,14 +1,19 @@
-const path = require('path')
-
-module.exports = async function(app, log) {
+module.exports = async function(app, log, config) {
   if (app.env === 'production') return
 
   log.trace('Running ESLint')
 
+  require('eslint-plugin-standard')
+  require('eslint-plugin-promise')
+  require('eslint-config-standard')
+
+  let eslintIgnoreFile = await app.glob('.eslintignore', { dirs: ['app', 'quadro'] })
+  let eslintrcFile = await app.glob('.eslintrc', { dirs: ['app', 'quadro'] })
+
   let CLIEngine = require('eslint').CLIEngine
   let cli = new CLIEngine({
-    ignorePath: path.resolve(__dirname, '../.eslintignore'),
-    configFile: path.resolve(__dirname, '../.eslintrc')
+    ignorePath: eslintIgnoreFile[0],
+    configFile: eslintrcFile[0]
   })
   let report = cli.executeOnFiles([app.appDir])
   let formatter = cli.getFormatter('compact')
