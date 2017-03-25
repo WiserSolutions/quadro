27a20a5010c398f9498243504ea59db4894c074e
Igor Shapiro <shapigor@gmail.com>
2017-03-25 00:56:22 -0700

    + Added configuration for HTTP connection response timeout * Refactored httpServer registration

--- services/http_server.js
+++ services/http_server.js
@@ -1,1 +1,4 @@
-module.exports = require('../lib/http_server/server')
+module.exports = async function(container) {
+  let httpApp = await container.create(require('../lib/http_server/server'))
+  return await httpApp.listen()
+}
