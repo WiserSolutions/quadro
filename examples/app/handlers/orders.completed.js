3c809b097308e01e1307eef60a2efec2e58f130c
Igor Shapiro <shapigor@gmail.com>
2017-03-26 20:13:09 -0700

    Message handlers now have dependencies injected
    resolves #63

--- examples/app/handlers/orders.completed.js
+++ examples/app/handlers/orders.completed.js
@@ -1,7 +1,9 @@
-module.exports = async function() {
-  let message = this.message
-  this.log.info('Got a message')
-  if (!message.orderId) throw new Error('No orderId')
-  if (message.orderId === 'retry') return this.retryAfterSec(60)
-  this.success()
+module.exports = function Handler(log) {
+  this.handle = async function(ctx) {
+    let message = ctx.message
+    log.info(message, 'Got a message')
+    if (!message.orderId) throw new Error('No orderId')
+    if (message.orderId === 'retry') return ctx.retryAfterSec(60)
+    ctx.success()
+  }
 }
