0175a697bf800a87ebf54fe2b2d080d9850be2c2
Igor Shapiro <shapigor@gmail.com>
2017-04-11 10:53:29 -0700

    + Cache service (resolves #96) + Option to cache config values returned from providers (resolves #92)

---
+++ services/cache.js
@@ -0,0 +1,36 @@
+module.exports = class {
+  constructor(redis) {
+    this.redis = redis
+  }
+
+  async get(key) {
+    this.validateKey(key)
+    let value = await this.redis.getAsync(`cache:${key}`)
+    if (value === null) return undefined
+    return value
+  }
+
+  async set(key, value) {
+    this.validateKey(key)
+    if (value === undefined) {
+      Q.log.warn(`Attempt to cache 'undefined' as the value of key ${key}. Will be stored as null`)
+    }
+    await this.redis.setAsync(`cache:${key}`, value || null)
+  }
+
+  async invalidate(key) {
+    this.validateKey(key)
+    await this.redis.delAsync(`cache:${key}`)
+  }
+
+  async clear() {
+    await Promise.map(
+      this.redis.keysAsync('cache:*'),
+      _ => this.redis.delAsync(_)
+    )
+  }
+
+  validateKey(key) {
+    if (!key) throw new Q.Errors.InvalidArgumentError({ key })
+  }
+}
