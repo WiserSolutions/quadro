e4ed27934eb434bac00db416a98ef11f822d6c68
Igor Shapiro <shapigor@gmail.com>
2017-05-31 15:46:32 -0700

    Implemented Repository `delete` and `get`
    - Added coverage files to .gitignore

--- examples/app/test/odbm/repository_test.js
+++ examples/app/test/odbm/repository_test.js
@@ -1,9 +1,7 @@
 /* eslint no-unused-expressions: 0 */
 
 describe('Model Repository', function() {
-  class TestAdapter {
-
-  }
+  class TestAdapter {}
   const testAdapter = new TestAdapter()
   const User = Q.Model('user')
   const repo = new Q.Repository(User, testAdapter)
@@ -28,6 +26,30 @@ describe('Model Repository', function() {
       expect(testAdapter.update).to.be.calledWith('users', 123, {
         first_name: 'John'
       })
+    })
+  })
+
+  describe('delete', function() {
+    it('calls adapter destroy method', async function() {
+      testAdapter.delete = this.sinon.spy()
+      await repo.delete(new User({ id: 123 }))
+      expect(testAdapter.delete).to.be.calledWith('users', 123)
+    })
+  })
+
+  describe('get', function() {
+    it('calls `adapter.get`', async function() {
+      testAdapter.get = this.sinon.stub()
+      await repo.get(1234)
+      expect(testAdapter.get).to.be.calledWith('users', 1234)
+    })
+
+    it('returns a model', async function() {
+      testAdapter.get = this.sinon.stub().callsFake(async () => {
+        return { first_name: 'John', id: 1234 }
+      })
+      let model = await repo.get(1234)
+      expect(model).to.eql(new User({ id: 1234, firstName: 'John' }))
     })
   })
 
