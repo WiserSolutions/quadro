899a7609fe9835acd1d1f609808f760b8412e6b2
Ankur Jain <ajain@quadanalytix.com>
2017-08-24 22:34:54 -0700

    Reconnect of failures rabbitmq

--- examples/app/test/pubsub/pubsub_consumer_test.js
+++ examples/app/test/pubsub/pubsub_consumer_test.js
@@ -104,5 +104,41 @@ describe('pubsub', function() {
       expect(deadLetterEntries[0]).to.containSubset({attemptsMade: 6, maxAttempts: 5, messageType: 'orders.test.consumer', content: {hello: 'world'}})
       expect(deadLetterEntries[0].killedAt).to.not.be.null
     })
+
+    it.only('test reconnect', async function() {
+      await channel.assertExchange('orders.test.consumer', 'fanout', {durable: false})
+      await channel.bindQueue(QUEUE_NAME, 'orders.test.consumer', '')
+      let handler = {
+        handle: this.sinon.spy()
+      }
+      hubMessageProcessor.register('orders.test.consumer', handler)
+
+      // Send a message through pub sub
+      pubsub.publish('orders.test.consumer', { hello: 'world' })
+      await Promise.delay(200)
+      expect(handler.handle).to.have.been.calledWith(this.sinon.match.containSubset({message: {hello: 'world'}}))
+      let scheduledEntries = await scheduleCollection.find({}).toArray()
+      expect(scheduledEntries).to.be.empty
+      let deadLetterEntries = await deadLetterCollection.find({}).toArray()
+      expect(deadLetterEntries).to.be.empty
+
+      handler.handle.reset()
+      // Delete the queue and exchange
+      await channel.deleteQueue(QUEUE_NAME)
+
+      // Send a message through pub sub
+      pubsub.publish('orders.test.consumer', { hello: 'world' })
+      // Even a message is sent then it is not recieved by handler as queue is deleted
+      expect(handler.handle).to.not.have.been.calledWith(this.sinon.match.containSubset({message: {hello: 'world'}}))
+      // Wait for few millis for connection to break
+      await Promise.delay(100)
+      // Create the queue again
+      await channel.assertQueue(QUEUE_NAME)
+      await channel.bindQueue(QUEUE_NAME, 'orders.test.consumer', '')
+      handler.handle.reset()
+      pubsub.publish('orders.test.consumer', { hello: 'world' })
+      await Promise.delay(200)
+      expect(handler.handle).to.have.been.calledWith(this.sinon.match.containSubset({message: {hello: 'world'}}))
+    })
   })
 })
