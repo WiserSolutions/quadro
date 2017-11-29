ffbb6a65c496adad04c79311fa5ee2b4352e8447
Tomer Eskenazi <tomeresk@gmail.com>
2017-11-29 17:44:01 +0200

    statsd - added support for prefix and global tags

--- services/stats.js
+++ services/stats.js
@@ -3,7 +3,9 @@ const StatsdClient = require('statsd-client')
 module.exports = function(config) {
   return new StatsdClient({
     host: config.get('quadro.statsd.host'),
-    port: config.get('quadro.statsd.port'),
-    tcp: config.get('quadro.statsd.protocol') === 'tcp'
+    port: config.get('quadro.statsd.port', 8125),
+    tcp: config.get('quadro.statsd.protocol') === 'tcp',
+    prefix: config.get('quadro.statsd.prefix', ''),
+    tags: config.get('quadro.statsd.tags', {})
   })
 }
