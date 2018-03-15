830cad8ebe9b7cabbe68ed29d741fd892f4a6d57
Igor Shapiro <shapigor@gmail.com>
2018-03-15 16:58:42 -0700

    Extracted firehose logic into `firehose-writer` module

--- examples/app/test/framework/logging/firehose_stream_test.js
+++ examples/app/test/framework/logging/firehose_stream_test.js
@@ -22,12 +22,13 @@ describe('Firehose logging', function() {
 
     describe('the initializer', function() {
       it('adds a stream to logger', async function() {
+        QT.stubConfig(STREAM_NAME_KEY, 'some_stream')
         const match = this.sinon.match
 
         const log = { registerStream: this.sinon.spy() }
         await Q.container.create(FirehoseInit, { args: { log } })
         expect(log.registerStream)
-          .to.have.been.calledWith({ stream: match.instanceOf(Writable) })
+          .to.have.been.calledWith({ type: 'raw', stream: match.instanceOf(Writable) })
       })
     })
   })
