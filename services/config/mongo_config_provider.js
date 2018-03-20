86a374b8c5593478756ceba8a29d90c17448a93f
Igor Shapiro <shapigor@gmail.com>
2018-03-19 17:25:19 -0700

    lint fix

--- services/config/mongo_config_provider.js
+++ services/config/mongo_config_provider.js
@@ -1,6 +1,6 @@
 module.exports = function(mongo, container) {
   return function(collectionName) {
     const Provider = require('../../lib/config/mongo_config_provider')
-    return container.create(Provider, { args: { collectionName }})
+    return container.create(Provider, { args: { collectionName } })
   }
 }
