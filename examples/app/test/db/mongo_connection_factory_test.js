9f027a6496f20ad8b72f06ec1f6b3e4f781c4189
Igor Shapiro <shapigor@gmail.com>
2018-04-07 11:49:27 -0700

    fix(mongo): update mongodb driver to a fixed version, remove all the workarounds

--- examples/app/test/db/mongo_connection_factory_test.js
+++ examples/app/test/db/mongo_connection_factory_test.js
@@ -17,44 +17,10 @@ describe('MongoConnectionFactory', function() {
           .to.be.rejectedWith(Q.Errors.MongoConnectionError)
       }
     })
-  })
 
-  describe('_parseConnectionString', function() {
-    it('supports multiple hosts, no db, no options', function() {
-      expect(mcf._parseConnectionString('mongodb://host1.com,host2.com'))
-        .to.eql({
-          connectionString: 'mongodb://host1.com,host2.com'
-        })
-    })
-
-    it('supports multiple hosts + db, no options', function() {
-      expect(mcf._parseConnectionString('mongodb://host1.com,host2.com/hello'))
-        .to.eql({
-          connectionString: 'mongodb://host1.com,host2.com',
-          dbName: 'hello'
-        })
-    })
-
-    it('adds a `/` before options', function() {
-      expect(mcf._parseConnectionString('mongodb://host1.com:23/?replicaSet=123'))
-        .to.eql({
-          connectionString: 'mongodb://host1.com:23/?replicaSet=123'
-        })
-    })
-
-    it('supports multiple hosts + options, no db', function() {
-      expect(mcf._parseConnectionString('mongodb://host1.com,host2.com/?replicaSet=123'))
-        .to.eql({
-          connectionString: 'mongodb://host1.com,host2.com/?replicaSet=123'
-        })
-    })
-
-    it('supports multiple hosts + db + options', function() {
-      expect(mcf._parseConnectionString('mongodb://host1.com,host2.com/hello?replicaSet=123'))
-        .to.eql({
-          connectionString: 'mongodb://host1.com,host2.com/?replicaSet=123',
-          dbName: 'hello'
-        })
+    it('returns a db', async function() {
+      const db = mcf.connectToDB('mongodb://host/hello')
+      expect(db).to.be.ok
     })
   })
 })
