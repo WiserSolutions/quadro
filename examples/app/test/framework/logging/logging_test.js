26a44afbb980b0504f69cc91c7b75b01a98d77f6
Igor Shapiro <shapigor@gmail.com>
2018-04-23 21:41:35 -0700

    Extend logging API with .audit(), .event() and .metric()

--- examples/app/test/framework/logging/logging_test.js
+++ examples/app/test/framework/logging/logging_test.js
@@ -15,4 +15,70 @@ describe('Logging', function() {
       '     NestedError: because of this \n'
     ])
   })
+
+  describe('diagnostics streams', function() {
+    const { Writable } = require('stream')
+    class TestWriteStream extends Writable {
+      constructor() { super({ objectMode: true })}
+      _write(chunk, encoding, done) {
+        lastWrittenObject = chunk
+        done()
+      }
+    }
+
+    let lastWrittenObject
+
+    describe('audit()', function() {
+      it('writes audit entry', function() {
+        Q.log._auditLogger.addStream({ type: 'raw', stream: new TestWriteStream() })
+
+        Q.log.audit('order', 'SKU1', 'completed', { group: 'Kitchen', extra: { hello: 'world' } })
+        expect(lastWrittenObject).to.deep.include({
+          name: Q.app.name,
+          object: 'SKU1',
+          status: 'completed',
+          hostname: require('os').hostname(),
+          action: 'order',
+          group: 'Kitchen',
+          extra: { hello: 'world' }
+        })
+      })
+    })
+
+    describe('metric()', function() {
+      it('writes audit entry', function() {
+        Q.log._metricLogger.addStream({ type: 'raw', stream: new TestWriteStream() })
+
+        Q.log.metric('orders', { department: 'footwear' }, { count: 1, sum: 5 })
+        expect(lastWrittenObject).to.deep.include({
+          d: {
+            metric: 'orders',
+            department: 'footwear'
+          },
+          count: 1,
+          sum: 5,
+          hostname: require('os').hostname(),
+          name: Q.app.name
+        })
+      })
+    })
+
+    describe('event()', function() {
+      it('writes audit entry', function() {
+        Q.log._eventLogger.addStream({ type: 'raw', stream: new TestWriteStream() })
+
+        Q.log.event('order.completed', {
+          department: 'footwear',
+          userId: 'John1',
+          orderTotal: 123.4
+        })
+        expect(lastWrittenObject).to.deep.include({
+          messageType: 'order.completed',
+          department: 'footwear',
+          userId: 'John1',
+          orderTotal: 123.4
+        })
+      })
+    })
+  })
 })
