e9a8ca316f6385c133d69338a6912503e80fde34
andrii <andynew3@gmail.com>
2018-08-02 14:30:06 +0300

    initialize processor inside processor

--- initializers/run_services.js
+++ initializers/run_services.js
@@ -1,5 +1,4 @@
 module.exports = async function(httpServer,
-  hubMessageConsumer = 'pubsub:hubMessageConsumer',
   rabbitMessageAdapter = 'pubsub:rabbitMessageAdapter') {
   // Ensure HTTP server and message consumers are started
 }
