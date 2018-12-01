22bc96a463bc64832ee8b56b905bfc9be1498b8b
Igor Shapiro <igor.shapiro@lemonade.com>
2018-12-01 20:06:07 +0200

    statsd: automatically infer `service`, `env` and `env_id` tags

--- examples/app/test/services/stats_test.js
+++ examples/app/test/services/stats_test.js
@@ -1,6 +1,36 @@
 describe('stats', function() {
+  describe('default tags', function() {
+    let Stats
+
+    beforeEach(function() {
+      Stats = Q.container.getDefinition('stats')
+    })
+
+    it('adds env_id if env.id is set', async function() {
+      QT.stubConfig('env.id', 'hello')
+      const stats = await Q.container.create(Stats)
+      expect(stats.options.tags).to.containSubset({ env_id: 'hello' })
+    })
+
+    it('adds env', async function() {
+      const stats = await Q.container.create(Stats)
+      expect(stats.options.tags).to.containSubset({ env: 'test' })
+    })
+
+    it('uses env=env.STAGE if set', async function() {
+      process.env.STAGE = 'theStage'
+      const stats = await Q.container.create(Stats)
+      expect(stats.options.tags).to.containSubset({ env: 'theStage' })
+    })
+
+    it('adds service=app.name', async function() {
+      const stats = await Q.container.create(Stats)
+      expect(stats.options.tags).to.containSubset({ service: 'quadro_demo_app' })
+    })
+  })
+
   it('is a valid object', async function() {
-    let statsd = await Q.container.getAsync('stats')
+    const statsd = await Q.container.getAsync('stats')
     expect(typeof statsd.increment).to.eql('function')
     expect(typeof statsd.gauge).to.eql('function')
     expect(typeof statsd.timing).to.eql('function')
--- services/stats.js
+++ services/stats.js
@@ -1,11 +1,26 @@
 const StatsdClient = require('statsd-client')
 
-module.exports = function(config) {
+module.exports = function(config, app) {
+  function buildDefaultTags() {
+    const tags = {}
+
+    const envId = config.get('env.id')
+    if (envId) tags.env_id = envId
+
+    const env = process.env.STAGE || app.env
+    if (env) tags.env = env
+
+    const service = app.name
+    if (service) tags.service = service
+
+    return tags
+  }
+
   return new StatsdClient({
     host: config.get('quadro.statsd.host'),
     port: config.get('quadro.statsd.port', 8125),
     tcp: config.get('quadro.statsd.protocol') === 'tcp',
     prefix: config.get('quadro.statsd.prefix', ''),
-    tags: config.get('quadro.statsd.tags', {})
+    tags: config.get('quadro.statsd.tags', buildDefaultTags())
   })
 }
