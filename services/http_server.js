7736a25844b3627c2d32e857214e9bd1079d66a3
Igor Shapiro <igor.shapiro@lemonade.com>
2018-12-28 21:28:36 +0200

    Add HTTP Server initializer

--- services/http_server.js
+++ services/http_server.js
@@ -1,8 +1,9 @@
 module.exports = async function(container, app, config) {
-  let httpApp = await container.create(require('../lib/http_server/server'))
-
   const isEnvWithoutServer = app.isREPL || app.isTask
   const forceListen = config.get('quadro.http.force')
 
-  if (!isEnvWithoutServer || forceListen) return httpApp.listen()
+  if (!isEnvWithoutServer || forceListen) {
+    const httpApp = await container.create(require('../lib/http_server/server'))
+    return await httpApp.listen()
+  }
 }
