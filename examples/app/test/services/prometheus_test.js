dfde9ddb2f8f721b5a5cb0178d842978765f7672
Matthew Conover <mconover@wiser.com>
2020-01-10 12:22:46 -0800

    Added server so prometheus can query metrics

--- examples/app/test/services/prometheus_test.js
+++ examples/app/test/services/prometheus_test.js
@@ -1,7 +1,16 @@
 /* eslint-disable no-unused-expressions */
+const http = require('http')
+
 describe('Prometheus Service', () => {
   it('exists', async () => {
     const prom = await Q.container.getAsync('prometheus')
     expect(prom).is.not.null
+  })
+
+  it('launches webserver', () => {
+    http.get({url: 'http://localhost/metrics', port: 9230}, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.body).to.contain('quadro_process_cpu_user_seconds_total')
+    })
   })
 })
