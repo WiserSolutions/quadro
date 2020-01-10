e08e6b377abbd040ccf34112af6d0e38110e2e53
Matthew Conover <mconover@wiser.com>
2020-01-10 12:22:46 -0800

    Added server so prometheus can query metrics

--- services/prometheus.js
+++ services/prometheus.js
@@ -1,5 +1,6 @@
 module.exports = function(config) {
   const client = require('prom-client')
+  const http = require('http')
 
   if (!config.get('prometheus.disableDefaultCollection')) {
     client.collectDefaultMetrics({
@@ -7,6 +8,15 @@ module.exports = function(config) {
       prefix: config.get('promethues.prefix', 'quadro_')
     })
   }
+
+  http.createServer((req, res) => {
+    if (req === '/metrics') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+        .end(client.register.metrics())
+    } else {
+      res.writeHead(404).end()
+    }
+  }).listen(config.get('prometheus.port', 9230))
 
   return client
 }
