550adb9e0808f3637ae0745dc829a68cd702769e
dbeal-wiser <50845286+dbeal-wiser@users.noreply.github.com>
2020-01-13 13:51:06 -0800

    Update prometheus_test.js

--- examples/app/test/services/prometheus_test.js
+++ examples/app/test/services/prometheus_test.js
@@ -7,10 +7,11 @@ describe('Prometheus Service', () => {
     expect(prom).is.not.null
   })
 
-  it('launches webserver', () => {
+  it('launches webserver', (callback) => {
     http.get({url: 'http://localhost/metrics', port: 9230}, (res) => {
       expect(res.statusCode).to.equal(200)
       expect(res.body).to.contain('quadro_process_cpu_user_seconds_total')
+      callback()
     })
   })
 })
