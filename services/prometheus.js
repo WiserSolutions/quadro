6cdf5b0bfaf070d7ce779e19d4b916175be6a57b
Matthew Conover <mconover@wiser.com>
2020-01-16 13:34:04 -0800

    Prometheus code cleanup

--- services/prometheus.js
+++ services/prometheus.js
@@ -1,7 +1,9 @@
-module.exports = function(config, cluster) {
-  const client = require('prom-client')
-  const http = require('http')
+const client = require('prom-client')
+const http = require('http')
 
+const aggr = new client.AggregatorRegistry()
+
+module.exports = function(config, cluster) {
   client.prefix = config.get('promethues.prefix', 'quadro_')
 
   if (!config.get('prometheus.disableDefaultCollection')) {
@@ -11,37 +13,40 @@ module.exports = function(config, cluster) {
     })
   }
 
-  http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/metrics') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end(client.register.metrics())
-    } else {
-      res.writeHead(404).end()
-    }
-  }).listen(config.get('prometheus.port', 9230))
-
+  http.createServer(metricsServerHandler)
+    .listen(config.get('prometheus.port', 9230))
   if (cluster.isMaster) {
-    const aggr = new client.AggregatorRegistry()
-
-    http.createServer((req, res) => {
-      if (req.method === 'GET' && req.url === '/cluster_metrics') {
-        aggr.clusterMetrics((err, metrics) => {
-          if (err) {
-            Q.log.warn(err)
-            res.writeHead(500, 'Content-Type', 'text/plain')
-            res.end(err.message)
-          } else {
-            res.writeHead(200, 'Content-Type', aggr.contentType)
-            res.end(metrics)
-          }
-        })
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end(client.register.metrics())
-      } else {
-        res.writeHead(404).end()
-      }
-    }).listen(config.get('prometheus.aggregatorPort', 9231))
+    http.createServer(aggregatedMetricsServerHandler)
+      .listen(config.get('prometheus.aggregatorPort', 9231))
   }
 
   return client
+}
+
+function metricsServerHandler(req, res) {
+  if (req.method === 'GET' && req.url === '/metrics') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end(client.register.metrics())
+  } else {
+    res.writeHead(404).end()
+  }
+}
+
+function aggregatedMetricsServerHandler(req, res) {
+  if (req.method === 'GET' && req.url === '/cluster_metrics') {
+    aggr.clusterMetrics((err, metrics) => {
+      if (err) {
+        Q.log.warn(err)
+        res.writeHead(500, 'Content-Type', 'text/plain')
+        res.end(err.message)
+      } else {
+        res.writeHead(200, 'Content-Type', aggr.contentType)
+        res.end(metrics)
+      }
+    })
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end(client.register.metrics())
+  } else {
+    res.writeHead(404).end()
+  }
 }
