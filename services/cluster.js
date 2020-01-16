7a881f705430d15e30294d4e09231d3633b03596
Matthew Conover <mconover@wiser.com>
2020-01-16 15:28:55 -0800

    Testing

--- services/cluster.js
+++ services/cluster.js
@@ -6,7 +6,7 @@ let initlized = false
 
 module.exports = class {
   // only a single instance should be consturcted.
-  constructor(app, config, log) {
+  constructor(app, config) {
     this.config = config
     this.numCPUs = os.cpus().length
     this.clusteringActive = (
@@ -16,45 +16,41 @@ module.exports = class {
     this.isMaster = cluster.isMaster
     this.workers = []
 
-    // don't actually start up the cluster
-    if (!this.clusteringActive) return
+    // go ahead and start things up
+    if (this.clusteringActive) this._init()
+  }
 
-    if (this.isMaster) {
-      if (initlized) return
-      initlized = true
-
-      log.debug(`Master ${process.pid} initlizing workers.`)
-      // start numCPUs - 1 workers.
-      for (let i = 0; i < this.numCPUs - 1; ++i) {
-        this.workers.push(cluster.fork())
-      }
-
-      cluster.on('exit', (worker, code, signal) => {
-        log.info(`Worker ${worker.process.pid} exited with code ${code} from signal ${signal}.`)
-        this.shutdown(1)
-      })
-      process.on('SIGINT', () => this.shutdown())
-      process.on('SIGTERM', () => this.shutdown())
-    } else {
-      log.info(`Worker ${process.pid} started.`)
+  _init() {
+    if (!this.isMaster) {
+      Q.log.info(`Worker ${process.pid} started.`)
+      return
     }
+
+    if (initlized) return
+    initlized = true
+
+    Q.log.debug(`Master ${process.pid} initlizing workers.`)
+    // start numCPUs - 1 workers.
+    for (let i = 0; i < this.numCPUs - 1; ++i) {
+      this.workers.push(cluster.fork())
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      Q.log.info(`Worker ${worker.process.pid} exited with code ${code} from signal ${signal}.`)
+      this.shutdown(1)
+    })
+    process.on('SIGINT', () => this.shutdown())
+    process.on('SIGTERM', () => this.shutdown())
   }
 
   shutdown(code = 0) {
     for (const w of this.workers) {
       if (w.isDead()) continue
       try {
-        w.process.kill()
-      } catch (err) {
-        Q.log.debug(err, 'Error sending sigterm to worker.')
-      }
-      if (w.isDead()) continue
-      try {
         w.process.kill('SIGKILL')
       } catch (err) {
         Q.log.error(err, 'Error killing worker!')
       }
-      if (!w.isDead()) Q.log.error('Worker still alive after attempting to kill!')
     }
 
     process.exit(code)
