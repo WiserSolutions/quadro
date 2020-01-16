bb5900baf0fb1688919bc326b8345a64eb0c1ce6
Matthew Conover <mconover@wiser.com>
2020-01-16 14:36:04 -0800

    Better exit handling

--- services/cluster.js
+++ services/cluster.js
@@ -11,6 +11,7 @@ module.exports = class {
     this.numCPUs = os.cpus().length
     this.clusteringActive = this.config.get('quadro.clustering', false)
     this.isMaster = cluster.isMaster
+    this.workers = []
 
     // don't actually start up the cluster
     if (!this.clusteringActive) return
@@ -18,11 +19,32 @@ module.exports = class {
     if (this.isMaster) {
       if (initlized) return
       initlized = true
+
       log.debug(`Master ${process.pid} initlizing workers.`)
       // start numCPUs - 1 workers.
-      for (let i = 0; i < this.numCPUs - 1; ++i) cluster.fork()
+      for (let i = 0; i < this.numCPUs - 1; ++i) {
+        this.workers.push(cluster.fork())
+      }
+
       cluster.on('exit', (worker, code, signal) => {
         log.info(`Worker ${worker.process.pid} exited with code ${code} from signal ${signal}.`)
+        for (const w of this.workers) {
+          if (w.isDead()) continue
+          try {
+            w.process.kill()
+          } catch (err) {
+            log.debug(err, 'Error sending sigterm to worker.')
+          }
+          if (w.isDead()) continue
+          try {
+            w.process.kill('SIGKILL')
+          } catch (err) {
+            log.error(err, 'Error killing worker!')
+          }
+          if (!w.isDead()) log.error('Worker still alive after attempting to kill!')
+        }
+
+        process.exit(1)
       })
     } else {
       log.info(`Worker ${process.pid} started.`)
