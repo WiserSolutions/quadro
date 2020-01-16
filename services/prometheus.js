7acdc701001bd6b8c02906a6f0bfad227f2a59af
Matthew Conover <mconover@wiser.com>
2020-01-16 13:27:32 -0800

    Added prometheus support for clustering and aggregation

--- services/prometheus.js
+++ services/prometheus.js
@@ -1,4 +1,4 @@
-module.exports = function(config) {
+module.exports = function(config, cluster) {
   const client = require('prom-client')
   const http = require('http')
 
@@ -19,6 +19,29 @@ module.exports = function(config) {
       res.writeHead(404).end()
     }
   }).listen(config.get('prometheus.port', 9230))
+
+  if (cluster.isMaster) {
+    const aggr = new client.AggregatorRegistry()
+
+    http.createServer((req, res) => {
+      if (req.method === 'GET' && req.url === '/cluster_metrics') {
+        aggr.clusterMetrics((err, metrics) => {
+          if (err) {
+            Q.log.warn(err)
+            res.writeHead(500, 'Content-Type', 'text/plain')
+            res.end(err.message)
+          } else {
+            res.writeHead(200, 'Content-Type', aggr.contentType)
+            res.end(metrics)
+          }
+        })
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end(client.register.metrics())
+      } else {
+        res.writeHead(404).end()
+      }
+    }).listen(config.get('prometheus.aggregatorPort', 9231))
+  }
 
   return client
 }
