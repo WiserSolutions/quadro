88034a08141028c38de5c76b6fd34f82957407ae
Matthew Conover <mconover@wiser.com>
2020-05-15 14:04:15 -0700

    Github actions

--- examples/app/test/framework/logging/logging_test.js
+++ examples/app/test/framework/logging/logging_test.js
@@ -9,11 +9,9 @@ describe('Logging', function() {
     let err = new TopLevelError('something happened', { a: 1 }, new NestedError('because of this'))
     let output = STDOUT.inspectSync(function() {
       Q.log.error({ err })
-    }).map(stripAnsi)
-    expect(output).to.containSubset([
-      ' TopLevelError: something happened ( a: 1 )\n',
-      '     NestedError: because of this \n'
-    ])
+    }).map(stripAnsi).join('')
+    expect(output).to.have.string('TopLevelError: something happened')
+    expect(output).to.have.string('NestedError: because of this')
   })
 
   describe('diagnostics streams', function() {
